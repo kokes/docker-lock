88b07b671e6d3f0eb4b29b2be7336e6bbdcbf71f
Michael Perel <michaelsethperel@gmail.com>
2020-11-24 15:53:17 -0500

    better print

--- pkg/kind/kind.go
+++ pkg/kind/kind.go
@@ -3,7 +3,7 @@ package kind
 type Kind string
 
 const (
-	Dockerfile     = "Dockerfile"
-	Composefile    = "Composefile"
-	Kubernetesfile = "Kubernetesfile"
+	Dockerfile     = "docker"
+	Composefile    = "docker-compose"
+	Kubernetesfile = "kubernetes"
 )

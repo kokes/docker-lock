62abe3a29118527dbd4764a9fb1dc36c666f868a
Michael Perel <michaelsethperel@gmail.com>
2020-11-24 18:30:26 -0500

    added docker formatter

--- pkg/generate/format/docker.go
+++ pkg/generate/format/docker.go
@@ -2,6 +2,7 @@ package format
 
 import (
 	"sort"
+	"sync"
 
 	"github.com/safe-waters/docker-lock/pkg/generate/parse"
 	"github.com/safe-waters/docker-lock/pkg/kind"
@@ -9,6 +10,13 @@ import (
 
 type dockerfileImageFormatter struct {
 	kind kind.Kind
+}
+
+type formattedDockerfileImage struct {
+	Name     string `json:"name"`
+	Tag      string `json:"tag"`
+	Digest   string `json:"digest"`
+	position int
 }
 
 func NewDockerfileImageFormatter(kind kind.Kind) IImageFormatter {
@@ -19,25 +27,46 @@ func (d *dockerfileImageFormatter) Kind() kind.Kind {
 	return d.kind
 }
 
-func (d *dockerfileImageFormatter) FormatImages(images <-chan parse.IImage) ([]parse.IImage, error) {
-	var formattedImages []parse.IImage
+func (d *dockerfileImageFormatter) FormatImages(images <-chan parse.IImage) (map[string][]interface{}, error) {
+	formattedImages := map[string][]interface{}{}
 
 	for image := range images {
 		if image.Err() != nil {
 			return nil, image.Err()
 		}
 
-		formattedImages = append(formattedImages, image)
+		path := image.Metadata()["path"].(string)
+		position := image.Metadata()["position"].(int)
+
+		formattedImage := &formattedDockerfileImage{
+			Name:     image.Name(),
+			Tag:      image.Tag(),
+			Digest:   image.Digest(),
+			position: position,
+		}
+
+		formattedImages[path] = append(formattedImages[path], formattedImage)
 	}
 
-	sort.Slice(formattedImages, func(i int, j int) bool {
-		switch {
-		case formattedImages[i].Metadata()["path"].(string) != formattedImages[j].Metadata()["path"].(string):
-			return formattedImages[i].Metadata()["path"].(string) < formattedImages[j].Metadata()["path"].(string)
-		default:
-			return formattedImages[i].Metadata()["position"].(int) < formattedImages[j].Metadata()["position"].(int)
-		}
-	})
+	var waitGroup sync.WaitGroup
+
+	for _, images := range formattedImages {
+		waitGroup.Add(1)
+
+		go func() {
+			defer waitGroup.Done()
+
+			sort.Slice(images, func(i int, j int) bool {
+				image1 := images[i].(*formattedDockerfileImage)
+				image2 := images[j].(*formattedDockerfileImage)
+
+				return image1.position < image2.position
+			})
+		}()
+	}
+
+	waitGroup.Wait()
 
 	return formattedImages, nil
+
 }
--- pkg/generate/format/types.go
+++ pkg/generate/format/types.go
@@ -7,5 +7,5 @@ import (
 
 type IImageFormatter interface {
 	Kind() kind.Kind
-	FormatImages(images <-chan parse.IImage) ([]parse.IImage, error)
+	FormatImages(images <-chan parse.IImage) (map[string][]interface{}, error)
 }
